d975c9e01f97f66e29df5d2a719b9d91beafdedd
kr9277 <kr9277@bs.amalnet.k12.il>
2023-01-22 11:27:27 +0200

    a button

---
+++ app/src/main/java/com/example/one_botton/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.one_botton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button button;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button.setText("Oh, yea, ive been clicked!");
+            }
+        });
+    }
+}
